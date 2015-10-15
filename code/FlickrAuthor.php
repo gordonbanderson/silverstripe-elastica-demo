2694e78ea37487176207e45db10f1a559496bb6e
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-10-15 23:00:17 +0700

    FIX: Added additional fields

--- code/FlickrAuthor.php
+++ code/FlickrAuthor.php
@@ -1,23 +1,26 @@
 <?php
 	class FlickrAuthor extends DataObject {
 		private static $db = array(
-			'PathAlias' => 'Varchar',
-			'DisplayName' => 'Varchar'
+			'PathAlias' => 'Varchar(255)',
+			'Username' => 'Varchar(255)',
+			'RealName' => 'Varchar(255)',
+			'Location' => 'Varchar(255)',
+			'NSID' => 'Varchar(255)'
 		);
 
 		private static $has_many = array('FlickrPhotos' => 'FlickrPhoto');
-
 
 		private static $summary_fields = array(
 			'PathAlias' => 'URL',
 			'DisplayName' => 'Display Name'
 		);
 
-
 		/**
 		 * A search is made of the path alias during flickr set import
 		 */
 		private static $indexes = array(
-			'PathAlias' => true
+			'PathAlias' => true,
+			'RealName' => true,
+			'Username' => true
 		);
 	}
